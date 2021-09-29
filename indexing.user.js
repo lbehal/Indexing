b21f09fe6adb8cfa5fbd43b61fc576e27b9de1f3
lbehal <lbehal@users.noreply.github.com>
2021-09-29 14:10:51 +0200

    zoom

--- indexing.user.js
+++ indexing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Indexing 
 // @namespace    Indexing
-// @version 1.3
+// @version 1.4
 // @description  Improves CZ indexing
 // @author       LB
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js 
@@ -25,3 +25,16 @@ $1("#Form_A100816 > tbody > tr > td.FieldB > span > a").each(function() {
      $1(this).text(czhref);
     
 });
+if(window.location.href.indexOf("eur-document") != -1)
+{
+	$1("div.SiteTitle").each(
+		function()
+		{
+			var zoomSelect = $(`<div><span>Zoom</span><select onchange="$('div.PageBox')[0].style.zoom = this.value;">
+     <option value="1" selected="selected">100%</option>
+     <option value="1.5">150%</option>
+     <option value="2">200%</option>
+</select></div>`);
+				zoomSelect.insertBefore($(this));
+			});
+}
