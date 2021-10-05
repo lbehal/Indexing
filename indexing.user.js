ab34298b880dc1bf8acebc4a2d0c43caf624b400
lbehal <lbehal@users.noreply.github.com>
2021-10-05 14:15:51 +0200

    zoom radio

--- indexing.user.js
+++ indexing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Indexing 
 // @namespace    Indexing
-// @version 1.7
+// @version 1.8
 // @description  Improves CZ indexing
 // @author       LB
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js 
@@ -11,35 +11,43 @@
 // ==/UserScript==
 /* jshint -W097 */
 
+
+
 $1 = this.jQuery = jQuery.noConflict(true);
 var debug = function(x) {    
-    console.log(x); 
+	console.log(x);
 };
 
 $1("#Form_A100816 > tbody > tr > td.FieldB > span > a").each(function() {    
-    var href = $1(this).attr("href");
-    debug(href);
-    var czhref= href.replace("/SK/", "/CS/").replace("locale=sk","locale=cs");
-    debug("changed to :"+ czhref);
-    $1(this).attr("href", czhref);
-     $1(this).text(czhref);
-    
+	var href = $1(this).attr("href");
+	debug(href);
+	var czhref= href.replace("/SK/", "/CS/").replace("locale=sk","locale=cs");
+	debug("changed to :"+ czhref);
+	$1(this).attr("href", czhref);
+	$1(this).text(czhref);
+
 });
 if(window.location.href.indexOf("eur-document") != -1)
 {
 	$1("div.SiteTitle").each(
 		function()
 		{
-			var zoomSelect = $(`<div><span>Zoom</span><select onchange="{$('div.PageBox')[0].style.zoom = this.value;$($('div.PageBox')[0]).css('line-height','150%');}">
-     <option value="1" selected="selected">100%</option>
-      <option value="1.25">125%</option>
-     <option value="1.5">150%</option>
-      <option value="1.75">175%</option>
-     <option value="2">200%</option>
-</select></div>`);
-				zoomSelect.insertBefore($(this));
-			});
+			var zoomSelect = $(`<div style='margin: 5px;'><span>Zoom</span><label><input type='radio' name='editList' value='1' checked />100%</label>
+<label><input type='radio' name='editList' value='1.25' />125%</label>
+<label><input type='radio' name='editList' value='1.5' />150%</label>
+<label><input type='radio' name='editList' value='1.75' />175%</label>
+<label><input type='radio' name='editList' value='2' />200%</label>
+</div>`);
+			zoomSelect.insertBefore($(this));
+		});
+
+	$(document).on('click', '[name="editList"]', function () {
+
+		$('div.PageBox')[0].style.zoom =$(this).val();
+		$($('div.PageBox')[0]).css('line-height','150%');
+	});
 }
 $("p.MsoFootnoteText[style*='margin-right:-']").each(function() {
-$1(this).attr('style', '');
+	$1(this).attr('style', '');
 });
+
